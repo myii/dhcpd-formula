6df11172ed9d74d7cd988734897de0d23f493700
Imran Iqbal <iqbalmy@hotmail.com>
2020-04-08 16:29:41 +0100

    test(ldap): add all tests

---
+++ test/integration/ldap/controls/config_spec.rb
@@ -0,0 +1,103 @@
+# Default values
+config_filename = '/etc/dhcp/dhcpd.conf'
+rootgroup = 'root'
+# Overide by platform
+case platform[:family]
+when 'debian'
+  service_config_filename = '/etc/default/isc-dhcp-server'
+  service_config_file_contents = <<~SERVICE_CONFIG_FILE.chomp
+    # SaltStack-generated demon configuration file for ISC dhcpd
+
+    # Path to dhcpd's config file (default: /etc/dhcp/dhcpd.conf).
+    #DHCPD_CONF=/etc/dhcp/dhcpd.conf
+
+    # Path to dhcpd's PID file (default: /var/run/dhcpd.pid).
+    #DHCPD_PID=/var/run/dhcpd.pid
+
+    # Additional options to start dhcpd with.
+    #       Don't use options -cf or -pf here; use DHCPD_CONF/ DHCPD_PID instead
+    #OPTIONS=""
+
+    # On what interfaces should the DHCP server (dhcpd) serve DHCP requests?
+    #       Separate multiple interfaces with spaces, e.g. "eth0 eth1".
+    INTERFACES="em1 em2"
+  SERVICE_CONFIG_FILE
+when 'redhat', 'fedora'
+  service_config_filename = '/etc/systemd/system/dhcpd.service.d/override.conf'
+  service_config_file_contents = <<~SERVICE_CONFIG_FILE.chomp
+    [Service]
+    ExecStart=
+    ExecStart=/usr/sbin/dhcpd -f -cf /etc/dhcp/dhcpd.conf -user dhcpd -group dhcpd --no-pid em1 em2
+  SERVICE_CONFIG_FILE
+when 'suse'
+  config_filename = '/etc/dhcpd.conf'
+when 'freebsd'
+  config_filename = '/usr/local/etc/dhcpd.conf'
+  rootgroup = 'wheel'
+  service_config_filename = '/etc/rc.conf.d/dhcpd'
+  service_config_file_contents = <<~SERVICE_CONFIG_FILE.chomp
+    # SaltStack-generated demon configuration file for ISC dhcpd
+
+    dhcpd_ifaces="em1 em2"
+  SERVICE_CONFIG_FILE
+when 'linux'
+  case platform[:name]
+  when 'arch'
+    config_filename = '/etc/dhcpd.conf'
+  end
+end
+
+control 'DHCPD configuration' do
+  title 'should be generated properly'
+
+  describe file(config_filename) do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into rootgroup }
+    its('mode') { should cmp '0644' }
+    its('content') do
+      should include <<~CONFIG_FILE.chomp
+        # dhcpd.conf
+        #
+        # SaltStack-generated configuration file for ISC dhcpd
+        #
+
+        # Customized dhcp options
+
+        # option definitions common to all supported networks...
+
+        #use-host-decl-names off;
+
+        # Use this to enble / disable dynamic dns updates globally.
+        #ddns-update-style none;
+        #update-static-leases off;
+
+        # If this DHCP server is the official DHCP server for the local
+        # network, the authoritative directive should be uncommented.
+        #authoritative;
+
+        # Use this to send dhcp log messages to a different log file (you also
+        # have to hack syslog.conf to complete the redirection).
+      CONFIG_FILE
+    end
+  end
+end
+
+control 'DHCPD service configuration' do
+  title 'should be generated properly'
+
+  only_if(
+    'the service configuration file is only available on the Debian, RedHat ' \
+    '& FreeBSD platform families'
+  ) do
+    ['debian', 'redhat', 'freebsd'].include?(platform[:family])
+  end
+
+  describe file(service_config_filename) do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into rootgroup }
+    its('mode') { should cmp '0644' }
+    its('content') { should include service_config_file_contents }
+  end
+end
---
+++ test/integration/ldap/controls/yaml_dump_spec.rb
@@ -0,0 +1,143 @@
+# frozen_string_literal: true
+
+control 'DHCPD `map.jinja` YAML dump' do
+  title 'should contain the lines'
+
+  arch = 'amd64'
+  common01 = <<~COMMON.chomp
+    allow: []
+    arch: #{arch}
+    authoritative: false
+    classes: {}
+  COMMON
+  common02 = <<~COMMON.chomp
+    customized_options: {}
+    ddns_domainname: ''
+    ddns_update_style: ''
+    default_lease_time: 0
+    deny: []
+    domain_name: ''
+    domain_name_servers: []
+    domain_search: []
+  COMMON
+  common03 = <<~COMMON.chomp
+    failover_peers: {}
+    get_lease_hostnames: ''
+    hosts: {}
+    keys: {}
+    ldap_base_dn: dc=example,dc=com
+    ldap_debug_file: /var/log/dhcp-ldap-startup.log
+    ldap_method: dynamic
+    ldap_password: '{SSHA}5++yqs7UNz22kAYf7jboAmklhavVXahK'
+    ldap_port: 389
+    ldap_server: 127.0.0.1
+    ldap_username: cn=root,dc=example,dc=com
+    listen_interfaces:
+    - em1
+    - em2
+    log_facility: ''
+    lookup:
+      enable: false
+    max_lease_time: 0
+    one_lease_per_client: ''
+  COMMON
+  common04 = <<~COMMON.chomp
+    routers: []
+  COMMON
+  common05 = <<~COMMON.chomp
+    server_identifier: ''
+    server_name: ''
+  COMMON
+  common06 = <<~COMMON.chomp
+    shared_networks: {}
+    subnet_mask: ''
+    subnets: {}
+    update_static_leases: false
+    use_host_decl_names: false
+    zones: {}
+  COMMON
+  config = '/etc/dhcp/dhcpd.conf'
+  enable = 'false'
+  server = 'dhcp'
+  service = 'dhcpd'
+  yaml_dump = "---\n"
+  yaml_dump +=
+    case platform[:family]
+    when 'debian'
+      <<~YAML_DUMP.chomp
+        #{common01}
+        config: #{config}
+        #{common02}
+        enable: #{enable}
+        #{common03}
+        #{common04}
+        server: isc-dhcp-server
+        #{common05}
+        service: isc-dhcp-server
+        service_config: /etc/default/isc-dhcp-server
+        #{common06}
+      YAML_DUMP
+    when 'redhat', 'fedora'
+      <<~YAML_DUMP.chomp
+        #{common01}
+        config: #{config}
+        #{common02}
+        enable: #{enable}
+        #{common03}
+        #{common04}
+        server: #{server}
+        #{common05}
+        service: #{service}
+        service_config: /etc/systemd/system/dhcpd.service.d/override.conf
+        #{common06}
+      YAML_DUMP
+    when 'suse'
+      <<~YAML_DUMP.chomp
+        #{common01}
+        config: /etc/dhcpd.conf
+        #{common02}
+        enable: #{enable}
+        #{common03}
+        #{common04}
+        server: dhcp-server
+        #{common05}
+        service: dhcpd
+        #{common06}
+      YAML_DUMP
+    when 'freebsd'
+      <<~YAML_DUMP.chomp
+        #{common01}
+        config: /usr/local/etc/dhcpd.conf
+        #{common02}
+        enable: #{enable}
+        #{common03}
+        rootgroup: wheel
+        #{common04}
+        server: isc-dhcp43-server
+        #{common05}
+        service: isc-dhcpd
+        service_config: /etc/rc.conf.d/dhcpd
+        #{common06}
+      YAML_DUMP
+    when 'linux'
+      case platform[:name]
+      when 'arch'
+        <<~YAML_DUMP.chomp
+          #{common01}
+          config: /etc/dhcpd.conf
+          #{common02}
+          enable: #{enable}
+          #{common03}
+          #{common04}
+          server: dhcp
+          #{common05}
+          service: dhcpd4
+          #{common06}
+        YAML_DUMP
+      end
+    end
+
+  describe file('/tmp/salt_yaml_dump.yaml') do
+    its('content') { should include yaml_dump }
+  end
+end
